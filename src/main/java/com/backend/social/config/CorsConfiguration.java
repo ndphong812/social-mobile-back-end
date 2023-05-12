a8a0f7960dcb01d61a4bc183aa74110372473380
Duong Tan Thanh <dtthanh.work@gmail.com>
2023-05-12 17:39:50 +0700

    add: configuration cors and api resend confirmation email

---
+++ src/main/java/com/backend/social/config/CorsConfiguration.java
@@ -0,0 +1,17 @@
+package com.backend.social.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CrosConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowedHeaders("*");
+    }
+}
